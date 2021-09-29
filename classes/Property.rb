87ab023eb3a087cf34d9beaca96067705d24e62c
shane <shane_qi@hotmail.com>
2021-09-29 22:05:20 +1000

    change initialize @address

--- classes/Property.rb
+++ classes/Property.rb
@@ -1,12 +1,23 @@
+require_relative 'Landlord'
+require_relative 'Tenant'
+
+require 'securerandom'
+
 class Property
+    attr_accessor :property_id, :type, :address, :rent, :status, :landlord, :tenant
 
-    attr_accessor :type, :address, :rent, :status
-
-    def initialize(type, rent, status= "vacant")
+    def initialize(type, rent, landlord, tenant, address, status= 'vacant')
+        @property_id = SecureRandom.uuid
         @type = type
         @rent = rent
         @status = status
-        @address = {}
+        @address = {
+            street_number: address[:street_number],
+            street_name: address[:street_name],
+            suburb: address[:suburb]
+        }
+        @landlord = Landlord.new(landlord[:first_name], landlord[:last_name])
+        @tenant = Tenant.new(tenant[:first_name], tenant[:last_name])
     end
 
     def update_rent(weekly_rent)
@@ -17,12 +28,15 @@ class Property
         @status = status
     end
 
-    def update_address(street_number, street_name, suburb)
-        @address = {
-            street_number: street_number,
-            street_name: street_name,
-            suburb: suburb
-        }
+    def update_tenant(first_name, last_name)
+        @tenant = Tenant.new(first_name, last_name)
+    end
+
+    def print_list
+        if @tenant.first_name && @tenant.last_name
+            return "#{@address[:street_number]} #{@address[:street_name]}, #{@address[:suburb]}. #{@rent}/pw #{@status}. Tenant: #{@tenant.first_name} #{@tenant.last_name}. Landlord: #{@landlord.first_name} #{@landlord.last_name}"
+        else 
+            return "#{@address[:street_number]} #{@address[:street_name]}, #{@address[:suburb]}. #{@rent}/pw #{@status}. Tenant: Not available. Landlord: #{@landlord.first_name} #{@landlord.last_name}"
+        end
     end
 end
-
