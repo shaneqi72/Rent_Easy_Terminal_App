474faaa29cead2e2918b3ee44f028b7931fcb13a
shane <shane_qi@hotmail.com>
2021-09-30 22:21:40 +1000

    add on error handling function

--- main.rb
+++ main.rb
@@ -3,10 +3,18 @@ require 'pastel'
 require 'tty-prompt'
 require_relative './classes/PropertyList'
 require_relative './classes/UserInterface'
+require 'json'
 
 font = TTY::Font.new(:doom)
 pastel = Pastel.new
 puts pastel.blue(font.write("Rent Easy", letter_spacing: 1))
 
+begin
 user = UserInterface.new
 user.main_menu
+rescue JSON::ParserError
+    File.open('./data/properties.json', 'w') do |f|
+        f.write([])
+    end
+    retry
+end
