3f15891163b18f238cb8326a0f0798dbc36281df
shane <shane_qi@hotmail.com>
2021-09-30 22:01:58 +1000

    delete mannual testing comments no useful

--- main.rb
+++ main.rb
@@ -10,11 +10,3 @@ puts pastel.blue(font.write("Rent Easy", letter_spacing: 1))
 
 user = UserInterface.new
 user.main_menu
-
-# property_list = PropertyList.new
-# property_list.create_property('house', '$330', 'vacant', '5', 'hall street', 'milton', 'james', 'lee')
-# property_list.create_property('unit', '$430', 'vacant', '10', 'milton street', 'toowong', 'liang', 'wei')
-# prompt = TTY::Prompt.new
-# choice = prompt.select("What property to update", property_list.property_options)
-# File.write('./data/properties.json', JSON.pretty_generate(property_list))
-# p property_list
